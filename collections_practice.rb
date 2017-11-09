ecc165fde80492bb0397cf8b04bbebd63d32eb40
Jered Sweeney <sweeneyjered@gmail.com>
2017-11-09 02:19:05 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,5 +11,10 @@ def sort_array_char_count(list)
 end
 
 def swap_elements(array)
-  array [0], array[1], array[2] =array[0], array[2], array[1]
+  array [0], array[1], array[2] = array[0], array[2], array[1]
 end
+
+def reverse_array(array)
+  array.reverse
+end
+  
