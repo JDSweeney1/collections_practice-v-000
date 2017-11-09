a7e1b75ce205e9f2f0b08fdda6857367f3643e42
Jered Sweeney <sweeneyjered@gmail.com>
2017-11-09 02:27:42 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,10 +11,13 @@ def sort_array_char_count(list)
 end
 
 def swap_elements(array)
-  array [0], array[1], array[2] = array[0], array[2], array[1]
+  array[1], array[2] = array[2], array[1]
+  array
 end
 
 def reverse_array(array)
   array.reverse
 end
+  
+def kesha_maker
   
